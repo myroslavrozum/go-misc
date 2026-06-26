3711a06e439d71d30f6442b2fc6b87126d104bcb
Myroslav Rozum <myroslavrozum@users.noreply.github.com>
2026-06-26 17:52:51 -0400

    add flags

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
+import "flag"
+
 func main() {
-	//async()
-	//dirwatcher()
-	gradient()
+	asyncFlag := flag.Bool("async", false, "Run asynchronous whoth channels example")
+	dirWatcherFlag := flag.Bool("dirwatcher", false, "Run fsnotify example")
+	gradientFlag := flag.Bool("gradient", false, "Run gradient descent example")
+
+	flag.Parse()
+
+	if asyncFlag != nil && *asyncFlag {
+		async()
+	}
+
+	if dirWatcherFlag != nil && *dirWatcherFlag {
+		dirwatcher()
+	}
+
+	if gradientFlag != nil && *gradientFlag {
+		gradient()
+	}
 }
